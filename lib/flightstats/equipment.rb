9f1294c657e2eafcc2206b2e901351d229708f29
Andrey Isaev <cubes.office@gmail.com>
2018-04-05 19:43:09 +0300

    add support for equipment api

--- lib/flightstats/equipment.rb
+++ lib/flightstats/equipment.rb
@@ -6,5 +6,21 @@ module FlightStats
                   :jet,
                   :widebody,
                   :regional
+
+    @@base_path = "/flex/airlines/equipment/v1/json"
+
+    class << self
+      def all(options = {})
+        from_response API.get("#{base_path}/all", {}, options), 'airlines'
+      end
+
+      def by_iata_code(iata_code, options = {})
+        from_response API.get("#{base_path}/iata/#{iata_code}", {}, options), 'equipment'
+      end
+
+      def base_path
+        @@base_path
+      end
+    end
   end
 end
